80b83617583e80e01014d29b80e96d2cb9b5ec4e
TimurIbatulin <mel0072007@icloud.com>
2022-10-05 18:38:09 +0300

    На контроль

--- Program.cs
+++ Program.cs
@@ -2,25 +2,25 @@
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
 
-// Console.Write("Введите количество чисел - ");
-// int a = Convert.ToInt32(Console.ReadLine());
-// int [] arr = new int [a];
-// int z=1;
-// int x=0;
-// int count=0;
-// while (x<arr.Length)
-// {
-//     Console.Write($"Введите {z} чисело - ");
-//     arr[x] = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество чисел - ");
+int a = Convert.ToInt32(Console.ReadLine());
+int [] arr = new int [a];
+int z=1;
+int x=0;
+int count=0;
+while (x<arr.Length)
+{
+    Console.Write($"Введите {z} чисело - ");
+    arr[x] = Convert.ToInt32(Console.ReadLine());
    
-//     if (arr[x]>0)
-//     {
-//         count++;
-//     }
-//     x++;
-//     z++;
-// }
-// Console.WriteLine($"Вы ввели {a} чисел, из которых {count} чисел больше ноля");
+    if (arr[x]>0)
+    {
+        count++;
+    }
+    x++;
+    z++;
+}
+Console.WriteLine($"Вы ввели {a} чисел, из которых {count} чисел больше ноля");
 
 // Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
